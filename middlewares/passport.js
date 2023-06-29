914920c2eace8b49b0c4d0adbf18f8f3ce2b0494
Najat76 <safari76@hotmail.co.uk>
2023-06-29 15:40:07 +0300

    notFoundHandler while createUser

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,7 +1,7 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
-// const  = require("passport-jwt").Strategy;
-// const { fromAuthHeaderAsBearerToken } = require("passport-jwt").ExtractJwt;
+const JWTStrategy = require("passport-jwt").Strategy;
+const { fromAuthHeaderAsBearerToken } = require("passport-jwt").ExtractJwt;
 const config = require("../config/keys");
 const User = require("../models/User");
 const passport = require("passport");
@@ -25,19 +25,20 @@ exports.localStrategy = new LocalStrategy(
   }
 );
 
-// exports.jwtStrategy = new JWTStrategy(
-//   {
-//     jwtFromRequest: fromAuthHeaderAsBearerToken(),
-//     secretOrKey: config.JWT_SECRET,
-//   },
-async (jwtPayload, done) => {
-  if (Date.now() > jwtPayload.exp * 1000) {
-    return done(null, false);
+exports.jwtStrategy = new JWTStrategy(
+  {
+    jwtFromRequest: fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.JWT_SECRET,
+  },
+  async (jwtPayload, done) => {
+    if (Date.now() > jwtPayload.exp * 1000) {
+      return done(null, false);
+    }
+    try {
+      const user = await User.findById(jwtPayload._id);
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
   }
-  try {
-    const user = await User.findById(jwtPayload._id);
-    return done(null, user);
-  } catch (error) {
-    return done(error);
-  }
-};
+);
