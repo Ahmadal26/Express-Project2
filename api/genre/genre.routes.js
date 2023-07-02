bcd40557738b484c0bd087adee04793f84e951cf
Najat76 <safari76@hotmail.co.uk>
2023-07-02 18:00:19 +0300

    final noon saturday

--- api/genre/genre.routes.js
+++ api/genre/genre.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const Genre = require("../../models/Genre");
 const Movie = require("../../models/Movie");
-const { genreCreate } = require("./genre.controllers");
+const { createNewGenre } = require("./genre.controllers");
 const router = express.Router();
 const passport = require("passport");
 
-router.post("/", passport.authenticate("jwt", { session: false }), genreCreate);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  createNewGenre
+);
 module.exports = router;
